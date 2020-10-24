09604d9fbbd6fcc1ed47ef7457daad89f0ef5d56
Barbara Martinez <barbara.martinez@southworks.com>
2020-10-24 15:59:59 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import {Container, Typography} from '@material-ui/core';
+
+
+export default ()=> (<Container><Typography variant="h5">The app's name is kudamono, which means fruit in japanese</Typography></Container>)
