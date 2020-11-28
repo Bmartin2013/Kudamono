e5ee6c8785e3797174dba80dacfbab2fb698e2ea
Barbara Martinez <barbara.martinez@southworks.com>
2020-11-28 17:29:58 -0300

    Add lazy loading for routes

--- src/App.js
+++ src/App.js
@@ -2,20 +2,13 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
-
-import Home from './pages/Home';
-import Results from './pages/Results';
 import Layout from './components/Layout'
 import watermelon from './themes/watermelon'
 
 const App = ({ store }) => (
 	<Provider store={store}>
 	<ThemeProvider theme={watermelon}>
-		<Layout 
-			appName="Kudamono"
-			Home={Home}
-			Results={Results}>
-		</Layout>
+		<Layout appName="Kudamono"/>
 	</ThemeProvider>
 	</Provider>
 );
